a9d17c0c6cb215e41683c1d63679ccb117bd44de
Crystal <crystalwatkins428@gmail.com>
2020-09-02 17:26:42 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let name = "Crystal";
+console.log(name);
+
+// cannot be a reserved keyword
+// Should be meaningful names
+//cannot start with a number
+// cannot contain a space or hyphen- camelCase
+//are case sensitive
+
+
