863495877ea04c71967e78d4f595b6ab4a5ec394
Crystal <crystalwatkins428@gmail.com>
2020-09-03 16:27:26 -0400

    updated

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-let name = "Crystal";
-console.log(name);
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
 
-// cannot be a reserved keyword
-// Should be meaningful names
-//cannot start with a number
-// cannot contain a space or hyphen- camelCase
-//are case sensitive
+let name = "Crystal"; //String Literal
+let age = 29; //number literal
+let isApproved = true; //boolean literal
+let firstName = undefined;
+let lastName = null; 
+ 
+let person = {
+    name: "Crystal",
+    age: 29
+};
 
+//Dot Notation
+person.name= "John";
 
+// Bracket Notation
+let selection = "name";
+person[selection] = "Mary";
+
+console.log(person.name);
+
+let selectedColors = ["red", "blue"];
+console.log(selectedColors[0])
